3200f20583fec2fe8ab0ad9d89122964363d4a22
Mouhannad-amma <mouhannad-ama@hotmail.com>
2016-04-11 17:32:59 +0800

    add attributes to user table using devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :resource, :resource_name, :devise_mapping
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def resource_name
     :user
@@ -16,4 +17,24 @@ class ApplicationController < ActionController::Base
     @devise_mapping ||= Devise.mappings[:user]
   end
 
+  protected
+
+  # def configure_premitted_parameters
+  #   devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:first_name, :last_name, :username, :birthday, :email, :password, :remember_me)}
+  #   devise_parameter_sanitizer.for(:sign_in) {|u| u.permit(:email, :password, :remember_me)}
+  #   devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:first_name, :last_name, :username, :birthday, :about, :email, :password, :remember_me)}
+  # end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |user_params|
+      user_params.permit(:first_name, :last_name, :username, :birthday, :email, :password, :remember_me)
+    end
+
+    devise_parameter_sanitizer.for(:account_update) do |user_params|
+      user_params.permit(:first_name, :last_name, :username, :birthday, :about, :email, :password, :current_password, :remember_me)
+    end
+  end
+
+  
 end
+ 
